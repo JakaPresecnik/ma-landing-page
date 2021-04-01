de796bd4a2721d1f561cb454e197e4baba67d922
JakaPresecnik <jaka.presecnik@gmail.com>
2021-04-01 22:39:19 +0200

    added certificates

--- src/Components/Aboot.js
+++ src/Components/Aboot.js
@@ -1,7 +1,14 @@
 import SvgThree from './SvgThree';
-import skills from '../skills';
+import {skills, certificates} from '../skills';
+import { Fragment } from 'react';
+import { FaGlobe } from 'react-icons/fa';
 
 function Aboot () {
+
+    const openPic = (type, id) => {
+        const pic = document.getElementById(`${type}${id}`);
+        pic.classList.toggle('zoomIn');
+    }
     return (
         <>
             <section>
@@ -11,15 +18,37 @@ function Aboot () {
                     <p>...</p>
                 </div>
             </section>
-            <div className='works' style={{paddingBottom: '1em'}}>
-                <h2>Skills I'm comfortable with</h2>
-                <div className="skill-display">
-                    {skills.map(skill => (
-                        <p key={skill.name}>
-                            <span style={{paddingRight: "1em"}}><skill.icon size="1.2em" /></span> 
-                            <progress id={skill.name} max="100" value={skill.progress} ></progress> 
-                            <span style={{paddingLeft: "1em"}}>{skill.name}</span></p>
-                    ))}
+            <div className='about-sub' style={{paddingBottom: '1em'}}>
+                <div className="skills">
+                    <h2 title="...progress bar represents how I'm comfortable with each...">Skills</h2>
+                    <div className="skill-display">
+                        {skills.map(skill => (
+                            <p key={skill.name}>
+                                <span style={{paddingRight: "1em"}}><skill.icon size="1.2em" /></span> 
+                                <progress id={skill.name} max="100" value={skill.progress} ></progress> 
+                                <span style={{paddingLeft: "1em"}}>{skill.name}</span></p>
+                        ))}
+                    </div>
+                </div>
+                <div className="certificates">
+                    <h2>Certificates</h2>
+                    <div className="certificate-display">
+                        {Object.keys(certificates).map(type => (
+                            <Fragment key={type}>
+                                <h4>{type.toUpperCase()}</h4>
+                                <div className="certificate-type-display">
+                                    {certificates[type].map(certificate => (
+                                        <div className={type != 'nanodegree' ? "certificate" : "nanodegree"} key={certificate.name}>
+                                            <h5>{certificate.name}</h5>
+                                            <img onClick={() => openPic(type, certificate.id)} id={`${type}${certificate.id}`} src={certificate.img} alt={`Certificate for ${certificate.name}`} />
+                                            <a className='project-badge' href={certificate.link} alt='Link to official certificate' target="_blank" rel="noreferrer"><FaGlobe size="1.5em" /></a>
+                                        </div>
+                                    ))}
+                                </div>
+                                <br/>
+                            </Fragment>
+                       ))}
+                    </div>
                 </div>
             </div>
         </>
--- src/skills.js
+++ src/skills.js
@@ -1,7 +1,17 @@
 import { FaEthereum, FaHtml5, FaCss3Alt, FaReact, FaPython, FaAngular } from "react-icons/fa";
 import { SiJavascript, SiJquery, SiRedux, SiNodeDotJs,  SiPostgresql } from "react-icons/si";
+import RND from './img/RND.jpg';
+import FEND from './img/FEND.jpg';
+import IPND from './img/IPND.jpg';
+import sololearnHTML from './img/sololearnHTML.jpg';
+import sololearnCSS from './img/sololearnCSS.jpg';
+import sololearnJS from './img/sololearnJS.jpg';
+import sololearnJQ from './img/sololearnJQ.jpg';
+import sololearnSQL from './img/sololearnSQL.jpg';
+import sololearnReact from './img/sololearnReact.jpg';
+import hackerrankReact from './img/hackerrankReact.jpg';
 
-const skills = [
+export const skills = [
     {name: 'HTML', icon: FaHtml5, progress: 95},
     {name: 'CSS', icon: FaCss3Alt, progress: 95},
     {name:'Javascript', icon: SiJavascript, progress: 90},
@@ -15,4 +25,20 @@ const skills = [
     {name: 'Python', icon: FaPython, progress: 20}
 ]
 
-export default skills;
+export const certificates = {
+    nanodegree: [
+        {id: 3, name: 'React', img: RND, date: 'June, 2020', link: 'https://confirm.udacity.com/DUKMWR2D', syllabus: 'https://s3-us-west-2.amazonaws.com/udacity-printer/production/syllabus/syllabus-nd019-default-en-us.pdf'},
+        {id: 2, name: 'Front End Developer', img: FEND, date: 'January 2020', link: 'https://confirm.udacity.com/QPPLCMPJ', syllabus:'https://s3-us-west-2.amazonaws.com/udacity-printer/production/syllabus/syllabus-nd0011-default-en-us.pdf'},
+        {id: 1, name: 'Intro to Programming', img: IPND, date: 'June 2018', link: 'https://confirm.udacity.com/GZ5FS7AC', syllabus: 'https://s3-us-west-2.amazonaws.com/udacity-printer/production/syllabus/syllabus-nd000-4.0.0-en-us.pdf'},
+    ],
+    certificates: [
+        {id: 7, name: 'React (Basic)', img: hackerrankReact, date: 'February 2021', link: 'https://www.hackerrank.com/certificates/0c2b99ba15e8'},
+        {id: 6, name: 'React + Redux course', img: sololearnReact, date: 'July 2020', link: 'https://www.sololearn.com/Certificate/1097-26490/jpg/'},
+        {id: 5, name: 'SQL course', img: sololearnSQL, date: 'January, 2021', link: 'https://www.sololearn.com/Certificate/1060-26490/jpg/'},
+        {id: 4, name: 'jQuery course', img: sololearnJQ, date: 'December 2017', link:'https://www.sololearn.com/Certificate/1082-26490/jpg/'},
+        {id: 3, name: 'JavaScript course', img: sololearnJS, date: 'June 2020', link: 'https://www.sololearn.com/Certificate/1024-26490/jpg/'},
+        {id: 2, name: 'CSS course', img: sololearnCSS, date: 'September 2017', link: 'https://www.sololearn.com/Certificate/1023-26490/jpg/'},
+        {id: 1, name: 'HTML course', img: sololearnHTML, date: 'September 2017', link: 'https://www.sololearn.com/Certificate/1014-26490/jpg/'},
+
+    ]
+}
